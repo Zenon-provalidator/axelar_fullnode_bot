9760947a93bdd7fc2cd081a6d459701454153766
zenon-provalidator <zenon@provalidator.com>
2022-03-05 17:06:03 +0900

    gg

--- process/rpc.js
+++ process/rpc.js
@@ -3,21 +3,22 @@ const exec = require('child_process').execSync
 
 // block height
 const getRpcHeight = (async (coin) => {
+	let cmd = ``
 	switch (coin){
 		case "eth" :
-			let cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
+			cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
 			break
 		case "polygon" :
-			let cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
+			cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
 			break
 		case "fantom" :
-			let cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
+			cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
 			break
 		case "moonbeam" :
-			let cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
+			cmd = `height=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_blockNumber", "params":[]}' ${cfg.EXTERN_RPC_URL} | jq .result | tr -d '"') && printf '%d\n' $height`
 			break
 		case "avax" :
-			let cmd = `height=$(curl --location --request POST '${cfg.EXTERN_RPC_URL}' --header 'Content-Type: application/json' --data-raw '{ "jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 1}' | jq .result | tr -d '"') && printf '%d\n' $height`
+			cmd = `height=$(curl --location --request POST '${cfg.EXTERN_RPC_URL}' --header 'Content-Type: application/json' --data-raw '{ "jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 1}' | jq .result | tr -d '"') && printf '%d\n' $height`
 			break			
 	}
 	let res = await exec(cmd)
