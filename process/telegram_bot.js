23ee669bb4538b3dfe26d1fdbfb68dbf0bc9d4bf
zenon-provalidator <zenon@provalidator.com>
2022-03-12 20:40:52 +0900

    gg

--- process/telegram_bot.js
+++ process/telegram_bot.js
@@ -33,7 +33,7 @@ bot.command('server', (ctx) => {
 	try{
 		let blockHeight = typeof variables.blockHeight === undefined ? undefined : variables.blockHeight.toLocaleString()
 		let rpcHeight = typeof variables.rpcHeight === undefined ? undefined : variables.rpcHeight.toLocaleString()
-		let heightDiff = Math.parseInt(rpcHeight) - Math.parseInt(blockHeight)
+		let heightDiff = variables.rpcHeight - variables.blockHeight
 		ctx.reply(`${cfg.PROJECT_NAME}_${cfg.SERVER_NAME}_${cfg.SERVER_TYPE}\nMemory : ${variables.mem}%\nCpu : ${variables.cpu}%\nDisk : ${variables.disk}%\nLatestHeight : ${blockHeight}\nrpcHeight : ${rpcHeight}\nheightDiff : ${heightDiff.toLocaleString()}`)
 	} catch (err){
 		ctx.reply(err)
